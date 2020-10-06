9dfad3a782132ab08cc7e096905a2f1029c3f8d7
suzuki-sho <mrmyself.chun1000ten@gmail.com>
2020-10-06 12:13:14 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,22 @@
 class User < ApplicationRecord
     devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  
-  validates :password, confirmation: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/}
+      do
+      validates :first_name
+      validates :last_name
+    end
+
+    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+      validates :f_name_kana
+      validates :l_name_kana
+    end
+  end
+
   has_many :items
   has_many :purchases
 end
