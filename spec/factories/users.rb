85263d5b4c2f76a8ad4a625881bdc6ded318f600
suzuki-sho <mrmyself.chun1000ten@gmail.com>
2020-10-06 14:57:47 +0900

    ActiveHash追加＆FactoryBot設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,12 @@
 FactoryBot.define do
-  facftory :user do
+  factory :user do
     nickname              {"hotman"}
     email                 {"kkk@gmail.com"}
     password              {"999jjj"}
     password_confirmation {password}
     first_name            {"川村"}
     last_name             {"洋一"}
-    f_name_kana　　　　　　 {"カワムラ"}
-    l_name_kana　　　　　　 {"ヨウイチ"}
-    birthday　　　　　　　　 {}
+    f_name_kana           {"カワムラ"}
+    l_name_kana           {"ヨウイチ"}
   end
 end
