b1226084e9f029663e3033ec71ce8a3585bce3de
suzuki-sho <mrmyself.chun1000ten@gmail.com>
2020-10-07 13:48:56 +0900

    ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :price, :description, :user, :category_id, :state_id, :shipping_id, :area_id, :days_id)
+  end
 end
