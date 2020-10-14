54e665508b60e5f9a267b2ec027862d535e1b629
suzuki-sho <mrmyself.chun1000ten@gmail.com>
2020-10-14 16:17:04 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,21 @@ ActiveRecord::Schema.define(version: 2020_10_11_084125) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "f_name_kana", null: false
+    t.string "l_name_kana", null: false
+    t.date "birthday", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
